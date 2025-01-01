3595f0d6b4b856bbf5e8865b8846484bd757c747
M6K4N9R <zubchevskiy@gmail.com>
2025-01-01 16:39:42 +0100

    Download Insomnia Postman and Thunder Client for vscode

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+app.get("/", function (req, res) {
+  res.send("Hello from Node API");
+});
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
