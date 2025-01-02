29258dfca03e375f7952d4815a77d3c8de0af293
M6K4N9R <zubchevskiy@gmail.com>
2025-01-02 12:07:09 +0100

    Clean up

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function (req, res) {
-  res.send("Hello from Node API");
+  res.send("Yo");
 });
 
 app.listen(3000, () => {
