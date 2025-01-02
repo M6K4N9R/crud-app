aba9bbb80b5f9f1cc23cecc9419b23e2ab2b4a04
M6K4N9R <zubchevskiy@gmail.com>
2025-01-02 12:42:54 +0100

    Add references

--- app.js
+++ app.js
@@ -1,8 +1,28 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello Test')
-})
 
-app.listen(3000)
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// routes
+app.get("/", (req, res) {
+  res.json({mssg: "Welcome to the API"});
+});
+
+
+// mongoose
+//   .connect(
+//     "mongodb+srv://zubchevskiy:2UrOa75mBlNo5OwZ@node-projects-backend.9mzl5.mongodb.net/?retryWrites=true&w=majority&appName=node-projects-backend"
+//   )
+//   .then(() => {
+//     console.log("Connected to the databese");
+//   })
+//   .catch(() => {
+//     console.log("Connection failed");
+//     console.error();
+//   });
+
+// // ==== continue from 29min
---
+++ videos.js
@@ -0,0 +1,3 @@
+const mongoDBPlaylist = "https://www.youtube.com/watch?v=gGNquGHqpNI&list=PL4cUxeGkcC9h77dJ-QJlwGlZlTd4ecZOA&index=16"
+const continueAt = "Connecting to MongoDB 1min"
+
