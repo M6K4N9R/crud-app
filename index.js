7771030718736d6e6d144474f54ed26f9435d34c
M6K4N9R <zubchevskiy@gmail.com>
2025-01-09 13:24:34 +0100

    Remove app.js and connect to database

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
 
-app.get("/", function (req, res) {
-  res.send("Yo");
-});
-
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
 
+app.get("/", (req, res) => {
+  res.send("Hello From Node API server update");
+});
+
+
 mongoose
   .connect(
-    "mongodb+srv://zubchevskiy:2UrOa75mBlNo5OwZ@node-projects-backend.9mzl5.mongodb.net/?retryWrites=true&w=majority&appName=node-projects-backend"
+    "mongodb+srv://zubchevskiy:2UrOa75mBlNo5OwZ@node-projects-backend.9mzl5.mongodb.net/"
   )
   .then(() => {
     console.log("Connected to the databese");
