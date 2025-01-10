f8f7b54d4b39b416692b71bdf1b390a535decad5
M6K4N9R <zubchevskiy@gmail.com>
2025-01-10 15:18:03 +0100

    Add Schema and Model. Connect to database

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const path = require("path");
 const bcrypt = require("bcrypt");
+const collection = require("./config");
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.set("view engine", "ejs");
 
 // static file
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
 app.get("/", (req, res) => {
   res.render("login");
